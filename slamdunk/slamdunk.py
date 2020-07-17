5b51471ed4daa9d2c711d56d0a37f2c74b4339d5
Jérémie Kalfon <jkobject@gmail.com>
2020-07-17 11:48:29 -0400

    different way to consider paired end

--- slamdunk/slamdunk.py
+++ slamdunk/slamdunk.py
@@ -417,8 +417,6 @@ def run():
     allparser = subparsers.add_parser('all', help='Run entire SLAMdunk analysis')
     allparser.add_argument('files', action='store', help='Single csv/tsv file (recommended) containing all sample files and sample info or a list of all sample BAM/FASTA(gz)/FASTQ(gz) files', nargs="+")
     allparser.add_argument("-r", "--reference", type=str, required=True, dest="referenceFile", help="Reference fasta file")
-    allparser.add_argument("-p", "--pairedend", type=bool, required=False, dest="paired", default=False, help="whether we are paired end seq or not")
-    allparser.add_argument("-r", "--reference", type=str, required=True, dest="referenceFile", help="Reference fasta file")
     allparser.add_argument("-b", "--bed", type=str, required=True, dest="bed", help="BED file with 3'UTR coordinates")
     allparser.add_argument("-fb", "--filterbed", type=str, required=False, dest="filterbed", help="BED file with 3'UTR coordinates to filter multimappers (activates -m)")
     allparser.add_argument("-o", "--outputDir", type=str, required=True, dest="outputDir", help="Output directory for slamdunk run.")
